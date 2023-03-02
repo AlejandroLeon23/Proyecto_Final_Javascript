d7914f1c296cb8894886d1de004dbfd4c5ba3fb1
AlejandroLeon23 <lic.alejandroleonlomeli>
2023-03-01 16:48:00 -0800

    Avance 2

--- js/app.js
+++ js/app.js
@@ -1,42 +1,51 @@
-// Crea la función totalIngresos
-const totalIngresos = () => {
-    let totalIngreso = 0;
-    for (let ingreso of Object.values(ingresos)) {
-      totalIngreso += ingreso;
-    }
-    return totalIngreso;
-  };
-  
-  // Crea la función totalEgresos
-  const totalEgresos = () => {
-    let totalEgreso = 0;
-    for (let egreso of Object.values(egresos)) {
-      totalEgreso += egreso;
-    }
-    return totalEgreso;
-  };
-  
-  // Crea la función cargarCabecero
-  const cargarCabecero = () => {
-    const presupuesto = totalIngresos() - totalEgresos();
-    const porcentajeEgreso = totalEgresos() / totalIngresos();
-    console.log(presupuesto);
-    console.log(porcentajeEgreso);
-    console.log(totalIngresos());
-    console.log(totalEgresos());
-  };
-  
-  // Define los arreglos
-  let egresos = {
-    Renta: 900,
-    Ropa: 400,
-  };
-  
-  let ingresos = {
-    Quincena: 9000,
-    Venta: 400,
-  };
-  
-  // Prueba la función cargarCabecero
-  cargarCabecero();
-  
+
+                // Define los arreglos
+                let egresos =[
+                  {
+                    nombre:"renta",
+                    valor: 900,
+                  },
+                  {
+                    nombre:"ropa",
+                    valor:400,
+                  }
+               ];
+
+                let ingresos = [
+                  {
+                    nombre:"Quincena",
+                    valor: 9000,
+                  },
+                  {
+                    nombre:"venta",
+                    valor:400,
+
+                  }
+                ];
+
+              // Crea la función totalIngresos
+              const totalIngresos = () => {
+                  let totalIngreso = 0;
+                  for (let ingreso of ingresos) {
+                    totalIngreso += ingreso.valor;
+                  }
+                  return totalIngreso;
+                };
+                
+              // Crea la función totalEgresos
+                const totalEgresos = () => {
+                  let totalEgreso = 0;
+                  for (let egreso of egresos) {
+                    totalEgreso += egreso.valor;
+                  }
+                  return totalEgreso;
+                };
+              // Crea la función cargarCabecero
+                const cargarCabecero = () => {
+                  const presupuesto = totalIngresos() - totalEgresos();
+                  const porcentajeEgreso = totalEgresos() / totalIngresos();
+                  console.log(presupuesto);
+                  console.log(porcentajeEgreso);
+                  console.log(totalIngresos());
+                  console.log(totalEgresos());
+                };
